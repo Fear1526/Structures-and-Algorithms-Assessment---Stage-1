c6f21e7bc0f6c714dc21c4b805539d87e5fed4b3
Fear1526 <feartheguns1526@gmail.com>
2022-03-20 18:18:05 +0000

    First Commit

---
+++ src/test/java/FamilyTreeUnitTest.java
@@ -0,0 +1,71 @@
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.jupiter.api.Test;
+
+class FamilyTreeUnitTest {
+
+    private FamilyTree underTest;
+
+
+    @Test
+    void shouldAddAncestorAndPartner(){
+        // given
+        underTest = new FamilyTree("James", "Mary");
+
+        //when
+        boolean expected = underTest.contains("James");
+        boolean expected2 = underTest.contains("Mary");
+
+        // then
+        assertTrue(expected);
+        assertTrue(expected2);
+    }
+
+    @Test
+    void shouldAddChild() throws Exception{
+        // given
+        underTest = new FamilyTree("James", "Mary");
+        underTest.addChild("John");
+        underTest.addChild("Amy");
+        underTest.addChild("Alan");
+
+        // when
+        boolean expected1 = underTest.contains("John");
+        boolean expected2 = underTest.contains("Amy");
+        boolean expected3 = underTest.contains("Alan");
+        boolean expected4 = underTest.contains("test");
+
+        // then
+        assertTrue(expected1);
+        assertTrue(expected2);
+        assertTrue(expected3);
+        assertFalse(expected4);
+    }
+
+    @Test
+    void toStringShouldHaveNoChildren(){
+        // given
+        underTest = new FamilyTree("James", "Mary");
+
+        // when
+        boolean expected = underTest.toString().contains("No Children");
+
+        // then
+        assertTrue(expected);
+    }
+
+    @Test
+    void toStringShouldHaveChildren() throws Exception{
+        // given
+        underTest = new FamilyTree("James", "Mary");
+        underTest.addChild("John");
+        underTest.addChild("Amy");
+        underTest.addChild("Alan");
+
+        // when
+        boolean expected = !underTest.toString().contains("no children");
+
+        // then
+        assertTrue(expected);
+    }
+}
