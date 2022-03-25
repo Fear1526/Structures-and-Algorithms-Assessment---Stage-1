b4bd12e4ee2fde34088cceb38f65ad1d15c4f751
Fear1526 <73917024+Fear1526@users.noreply.github.com>
2022-03-25 02:57:23 +0000

    Delete src directory

--- src/main/java/FamilyTreeADT.java
+++
@@ -1,6 +0,0 @@
-public interface FamilyTreeADT {
-    public class NotUniqueSiblingException extends RuntimeException {
-    }
-
-    public abstract void addChild(String name) throws NotUniqueSiblingException;
-}
